ffeafd5e9063c13ca2f18210948c174f57a075fc
Jhojany Uzcategui <jhojaforce@gmail.com>
2021-06-22 12:42:04 -0400

    no comments

--- js/index.js
+++ js/index.js
@@ -321,8 +321,9 @@ searching(idGlobal, keyGlobal); // first call to bring the elements and paint th
 
 // request for product categories to the server
 const HTMLResponseCategories = document.querySelector("#categories");
+const ext_URL = '/api/categories';
 
-data = fetchNow(ext_URL = '/api/categories', API_URL); // request for categories, by request fecth
+data = fetchNow(ext_URL, API_URL); // request for categories, by request fecth
 // End.
 
 // filling categories to the menu
